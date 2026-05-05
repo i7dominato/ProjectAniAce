529c6c217f542aae5c47148b62dca9c90433576c
Your Name <you@example.com>
2026-05-05 14:20:08 -0400

    Versão 0.5 (Beta)

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: "*" }));
 app.use(express.json());
 app.use("/episodes", episodeRoutes);
 app.use("/users", userRoutes);
