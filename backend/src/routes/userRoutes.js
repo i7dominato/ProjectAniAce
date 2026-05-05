00d1dcc30e7aac00bac0375ea853511a311fe997
Your Name <you@example.com>
2026-05-05 14:33:45 -0400

    Versão 0.5 (Beta)

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
-const { toggleFavorite } = require("../controllers/userController");
+
+const { toggleFavorite, saveProgress } = require("../controllers/userController");
 
 router.post("/favorite/:id", auth, toggleFavorite);
-
 router.post("/progress", auth, saveProgress);
 
 module.exports = router;
